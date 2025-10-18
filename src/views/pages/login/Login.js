7bdd055c058cc63d2fc7bb32a95e912710a64ae9
hungtmd <hunglq7@gmail.com>
2025-10-18 07:47:26 +0700

    18.10.2025

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -21,31 +21,24 @@ import { useRef, useState } from 'react'
 import Toasts from '../../../components/AppToasts'
 import { useEffect } from 'react'
 import { authService } from '../../../service/authService'
+import { useDispatch, useSelector } from 'react-redux'
 const Login = () => {
   const [toast, addToast] = useState(0)
   const toaster = useRef()
   const toasts = Toasts({ title: 'Thông báo', body: 'Tên đăng nhập hoặc mật khẩu không đúng' })
-  const [inputs, setInputs] = useState({
-    email: '',
-    password: '',
-  });
-  const { email, password } = inputs;
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
   const [submitted, setSubmitted] = useState(false);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await authService.loginWithUserCredentials(email, password)
-        console.log(response)
-        dispatch(listLogin(response.data))
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchData()
-  }, [])
+  const data = useSelector((state) => state.logins.data)
+
+  const handSummit = async () => {
+    const response = await authService.loginWithUserCredentials(email, password)
+    dispatch(listLogin(response.data))
+  }
 
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
+
       <CContainer>
         <CRow className="justify-content-center">
           <CCol md={8}>
@@ -59,7 +52,7 @@ const Login = () => {
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
                       </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
+                      <CFormInput placeholder="Username" autoComplete="username" value={email} onChange={(e) => setemail(e.target.value)} />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
@@ -69,11 +62,13 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
+                        value={password}
+                        onChange={(e) => setpassword(e.target.value)}
                       />
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4" onClick={() => addToast(toasts)}>
+                        <CButton color="primary" className="px-4" onClick={handSummit}>
                           Login
                         </CButton>
                         <CToaster ref={toaster} push={toast} placement="top-end" />
@@ -95,6 +90,7 @@ const Login = () => {
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                       tempor incididunt ut labore et dolore magna aliqua.
                     </p>
+                    <p>data: {JSON.stringify(data)}</p>
                     <Link to="/register">
                       <CButton color="primary" className="mt-3" active tabIndex={-1}>
                         Register Now!
