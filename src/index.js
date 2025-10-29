d72747ae6755e6ca713dc4c561404d876571a7e0
hungtmd <hunglq7@gmail.com>
2025-10-29 16:15:25 +0700

    16h15-29.10.2025

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import store from './store'
 // import 'primeicons/primeicons.css';
 // import { PrimeReactProvider } from 'primereact/api'
 // import 'primeflex/primeflex.css';
-import 'primereact/resources/primereact.css'
-import 'primereact/resources/themes/lara-light-indigo/theme.css'
+// import 'primereact/resources/primereact.css'
+// import 'primereact/resources/themes/lara-light-indigo/theme.css'
 
 
 createRoot(document.getElementById('root')).render(
